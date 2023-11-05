9522eeb939982690a817da6cd2f8a1e69d07aba5
Learning Code <Learn@APPLEs-MacBook-Pro-3.local>
2023-11-05 22:34:16 +0530

    logo update

--- app/components/Navbar/Drawerdata.tsx
+++ app/components/Navbar/Drawerdata.tsx
@@ -10,8 +10,6 @@ interface NavigationItem {
 const navigation: NavigationItem[] = [
   { name: 'Home', href: '/', current: true },
   { name: 'Courses', href: '#courses', current: false },
-  { name: 'Mentor', href: '#mentor', current: false },
-  { name: 'Group', href: '#/', current: false },
   { name: 'Testimonial', href: '#testimonial', current: false },
 ]
 
