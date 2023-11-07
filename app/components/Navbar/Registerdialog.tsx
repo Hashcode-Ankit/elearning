ff9ab9ab868d6da288ce44aceed17dfea8635771
Kanish <k4ni5h23@gmail.com>
2023-11-07 16:54:49 +0530

    fix

--- app/components/Navbar/Registerdialog.tsx
+++ app/components/Navbar/Registerdialog.tsx
@@ -33,7 +33,7 @@ const Register = (props) => {
 
   const sendToSlack = () => {
     // You should replace this URL with your Slack API endpoint.
-    const slackWebhookUrl = "https://hooks.slack.com/services/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"; // Replace with your Slack Webhook URL
+    const slackWebhookUrl = "https://hooks.slack.com/services/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"; // Replace with your Slack Webhook URL
 
     // Compose the message payload
     const messagePayload = {
