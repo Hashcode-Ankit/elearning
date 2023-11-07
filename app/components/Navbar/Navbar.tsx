bd0e3b184e24911cf53ca227e097633f67a13cfa
Kanish <k4ni5h23@gmail.com>
2023-11-07 15:51:35 +0530

    update nav

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -14,8 +14,9 @@ interface NavigationItem {
 }
 
 const navigation: NavigationItem[] = [
-    { name: 'Home', href: '#/', current: true },
+    { name: 'Home', href: '#', current: true },
     { name: 'Courses', href: '#courses', current: false },
+    { name: 'Mentor', href: '#mentor', current: false },
     { name: 'Testimonial', href: '#testimonial', current: false },
 ];
 
