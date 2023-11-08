3537b341df8530128f1d410a96acde95a8425caa
Kanish <k4ni5h23@gmail.com>
2023-11-08 11:04:16 +0530

    update testimonials

--- app/components/Navbar/Registerdialog.tsx
+++ app/components/Navbar/Registerdialog.tsx
@@ -97,6 +97,19 @@ const Register: React.FC<MyComponentProps> = (props) => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <div className="flex items-start justify-between rounded-t">
+                  <button type="button"
+                          className="text-gray-400 bg-transparent rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
+                          data-modal-hide="default-modal"
+                  onClick={closeModal}>
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="0 0 14 14">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                  </button>
+                  </div>
                   <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                     <div className="w-full max-w-md space-y-8">
                       <div>
