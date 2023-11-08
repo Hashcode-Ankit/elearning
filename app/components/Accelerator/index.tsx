c936610c8fd6f9a7af19e1d58dc569c4223465ad
Kanish <k4ni5h23@gmail.com>
2023-11-08 20:20:17 +0530

    update master session

---
+++ app/components/Accelerator/index.tsx
@@ -0,0 +1,28 @@
+"use client"
+import Slider from "react-slick";
+import React, { Component } from "react";
+import Image from "next/image";
+import Link from "next/link";
+export default class MultipleItems extends Component {
+
+    render() {
+        return (
+            <div className="py-20 sm:py-24 items-center" id="accelerator">
+                <div className='mx-auto max-w-2xl lg:max-w-7xl sm:py-4 px-4 lg:px-8 relative'>
+                    <h2 className="lh-82 text-midnightblue text-4xl md:text-55xl text-center md:text-start font-semibold">Explore our <br /> Career Restart Program.</h2>
+                </div>
+
+                <div className="flex justify-center items-center py-20 sm:static sm:inset-auto">
+                    <Link href="https://drive.google.com/file/d/1x50WR-WVwYPmwbJU3QMG3uH_L2W8X6JM/view">
+                        <button
+                          className="text-Blueviolet text-lg font-medium ml-9 py-5 px-16 transition duration-150 ease-in-out rounded-full bg-semiblueviolet hover:text-white hover:bg-Blueviolet"
+                        >
+                          Download Brochure
+                        </button>
+                    </Link>
+                </div>
+            </div>
+
+        );
+    }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Banner from './components/Banner/index';
 import Companies from './components/Companies/Companies';
 import Courses from './components/Courses/index';
 import Sessions from './components/Sessions/index';
+import Accelerator from './components/Accelerator/index';
 import Mentor from './components/Mentor/index';
 import Testimonials from './components/Testimonials/index';
 import Newsletter from './components/Newsletter/Newsletter';
@@ -14,6 +15,7 @@ export default function Home() {
       <Companies />
       <Courses />
       <Sessions />
+      <Accelerator />
       <Mentor />
       <Testimonials />
       <Newsletter />
