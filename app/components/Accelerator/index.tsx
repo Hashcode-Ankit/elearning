3645dac1d3893b5bedcd9062c0028c3fd4ac18fc
Kanish <k4ni5h23@gmail.com>
2023-11-09 01:11:36 +0530

    update master session

--- app/components/Accelerator/index.tsx
+++ app/components/Accelerator/index.tsx
@@ -7,12 +7,12 @@ export default class MultipleItems extends Component {
 
     render() {
         return (
-            <div className="py-20 sm:py-24 items-center" id="accelerator">
+            <div className="pt-4 pb-20 sm:py-24 items-center" id="accelerator">
                 <div className='mx-auto max-w-2xl lg:max-w-7xl sm:py-4 px-4 lg:px-8 relative'>
                     <h2 className="lh-82 text-midnightblue text-4xl md:text-55xl text-center md:text-start font-semibold">Explore our <br /> Career Restart Programs.</h2>
                 </div>
 
-                <div className="flex justify-center items-center pt-20 sm:static sm:inset-auto">
+                <div className="flex justify-center items-center pt-36 sm:static sm:inset-auto">
                     <Link target="_blank" href="https://drive.google.com/file/d/1x50WR-WVwYPmwbJU3QMG3uH_L2W8X6JM/view">
                         <button
                           className="text-Blueviolet text-lg font-medium ml-9 py-5 px-16 transition duration-150 ease-in-out rounded-full bg-semiblueviolet hover:text-white hover:bg-Blueviolet"
@@ -22,7 +22,7 @@ export default class MultipleItems extends Component {
                     </Link>
                 </div>
 
-                <div className="flex justify-end items-end pt-12 pr-20 sm:static sm:inset-auto">
+                <div className="flex justify-end items-end pt-20 pr-20 sm:static sm:inset-auto">
                     <Link href="" onClick={() => window.location.reload()} target="__blank" className="text-Blueviolet text-lg font-medium space-links">
                           ** get free career counselling before enrollment
                     </Link>
