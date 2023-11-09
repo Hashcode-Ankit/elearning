ace85382a56758e6c9683662d6f57e466b668fe1
Kanish <k4ni5h23@gmail.com>
2023-11-09 14:08:50 +0530

    fix css

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -16,8 +16,8 @@ interface NavigationItem {
 const navigation: NavigationItem[] = [
     { name: 'Home', href: '#/', current: true },
     { name: 'Courses', href: '#courses', current: false },
-    { name: 'Mentor', href: '#mentor', current: false },
-    { name: 'Testimonial', href: '#testimonial', current: false },
+    { name: 'Mentors', href: '#mentor', current: false },
+    { name: 'Testimonials', href: '#testimonial', current: false },
 ];
 
 function classNames(...classes: string[]) {
